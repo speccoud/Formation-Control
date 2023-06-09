c2c592a58871453bffe76dec52f8e5a1c7eae8f0
Samuel Peccoud <samuel@genofab.com>
2023-06-09 18:19:41 -0400

    Randomize agent starting location

--- src/Formation_Stationary.m
+++ src/Formation_Stationary.m
@@ -14,11 +14,33 @@ v           = 3;                       % path loss exponent
 r0          = 5;                       % reference antenna near-field
 PT          = 0.94;                    % reception probability threshold
 rho_ij      = 0;
+sim_speed   = 1;
+
+
+% Define the range of coordinates
+x_min = 0;
+x_max = 10;
+y_min = 0;
+y_max = 10;
+
+% Generate random positions for the swarm
+x_coords = x_min + (x_max - x_min) * rand(swarm_size, 1);
+y_coords = y_min + (y_max - y_min) * rand(swarm_size, 1);
+
+% Combine the x and y coordinates into a single matrix
+swarm = [x_coords, y_coords];
+
+for i = 1:swarm_size
+    for j = 1:2
+        fprintf("%d \n", swarm(i, j))
+    end
+    % print("\n")
+end
 
 
 % Initialize agents' position
-swarm = [-5,  14;   -5,  -19;   0,  0;   20, 20;
-         35,  -4;   68,    0;  72,  13;  72,-18];
+% swarm = [-5,  14;   -5,  -19;   0,  0;   20, 20;
+%          35,  -4;   68,    0;  72,  13;  72,-18; 10, 10; 40, 60;];
 
 communication_qualities = zeros(swarm_size, swarm_size);
 
@@ -56,6 +78,68 @@ drawnow                                         % Force a graphics refresh so th
 
 tic
 for k=1:max_times
+    
+
+
+%---Plot Swarms---%
+    figure(3);
+    dt = delaunayTriangulation(swarm(:, 1), swarm(:, 2));           % Compute the Delaunay triangulation
+    edgeIndex = edges(dt);                                         % Triangulation edge indices
+    % triplot(dt,'-o');                                               % Plot the Delaunay triangulation
+    
+    % triplot(dt,'o');                                               % Plot the Delaunay triangulation
+    
+    % Plot agents' directional vector arrows.
+    % vecarrow_x=swarm(:, 1)./sqrt(swarm(:, 1).^2+swarm(:, 2).^2);    % Plot Arrows
+    % vecarrow_y=swarm(:, 2)./sqrt(swarm(:, 1).^2+swarm(:, 2).^2);
+    % quiver(swarm(:, 1), swarm(:, 2), vecarrow_x, vecarrow_y, 0.3, 'r');
+    clf;
+    triplot(dt,'o');
+    for i = 1:swarm_size
+        for j= 1:swarm_size
+            if i ~= j && communication_qualities(i, j) > 0
+            hold on;
+
+             % Assign a different color to each line-label pair
+            color = rand(1, 3);  % Generate a random RGB color
+                
+            a1 = swarm(i,:);
+            a2 = swarm(j,:);
+            plot([a1(1), a2(1)], [a1(2), a2(2)], 'k-');
+    
+            % Calculate the position for the label
+            scaling_factor = 0.2;  % Adjust the scaling factor for the label positioning
+            midpoint_x = (a1(1) + a2(1)) / 2;
+            midpoint_y = (a1(2) + a2(2)) / 2;
+            displacement_x = (a2(1) - a1(1)) * scaling_factor;
+            displacement_y = (a2(2) - a1(2)) * scaling_factor;
+            label_x = midpoint_x + displacement_x;
+            label_y = midpoint_y + displacement_y + 2;
+            
+            % Add the number label at the midpoint of the line
+            label = communication_qualities(i, j);
+            label_str = sprintf('%.4f', label);
+
+            text(label_x, label_y, label_str, 'HorizontalAlignment', 'center');
+            hold off;
+
+            % Remove quality value for refresh
+            communication_qualities(i, j) = 0;
+            communication_qualities(j, i) = 0;
+            end
+            
+
+        end
+    end
+    axis equal;
+
+
+
+
+
+
+    communication_qualities = zeros(swarm_size, swarm_size);
+
     for i=1:swarm_size
         rho_ij=0;
         for j=[1:(i-1),(i+1):swarm_size]
@@ -69,12 +153,12 @@ for k=1:max_times
             end
 
             
-
+            
             phi_rij=gij*aij;
             communication_qualities(i,j) = phi_rij;
             qi=[swarm(i,1),swarm(i,2)];
             qj=[swarm(j,1),swarm(j,2)];
-            nd=(qi-qj)/sqrt(1+norm(qi-qj));
+            nd=(qi-qj)/sqrt(1+norm(qi-qj)*sim_speed);
 
             % Make the first agent have fixed velocity
             if i == 6
@@ -107,48 +191,7 @@ for k=1:max_times
 
     end
 
-    %---Plot Swarms---%
-    figure(3);
-    dt = delaunayTriangulation(swarm(:, 1), swarm(:, 2));           % Compute the Delaunay triangulation
-    edgeIndex = edges(dt);                                         % Triangulation edge indices
-    % triplot(dt,'-o');                                               % Plot the Delaunay triangulation
     
-    % triplot(dt,'o');                                               % Plot the Delaunay triangulation
-    
-    % Plot agents' directional vector arrows.
-    % vecarrow_x=swarm(:, 1)./sqrt(swarm(:, 1).^2+swarm(:, 2).^2);    % Plot Arrows
-    % vecarrow_y=swarm(:, 2)./sqrt(swarm(:, 1).^2+swarm(:, 2).^2);
-    % quiver(swarm(:, 1), swarm(:, 2), vecarrow_x, vecarrow_y, 0.3, 'r');
-    clf;
-    triplot(dt,'o');
-    for i = 1:swarm_size
-        
-        for j= 1:swarm_size
-            if i ~= j
-            hold on;
-            a1 = swarm(i,:);
-            a2 = swarm(j,:);
-            plot([a1(1), a2(1)], [a1(2), a2(2)], 'k-');
-
-            midpoint_x = (a1(1) + a2(1)) / 2;
-            midpoint_y = (a1(2) + a2(2)) / 2;
-            
-            % Add the number label at the midpoint of the line
-            label = communication_qualities(i, j);
-            text(midpoint_x, midpoint_y, num2str(label), 'HorizontalAlignment', 'center');
-            hold off;
-            end
-            
-
-        end
-    end
-
-    
-    
-    
-    
-
-    axis equal;
     pause(0)
 
 end
